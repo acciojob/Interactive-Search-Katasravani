9a1d2881bba7971ffd4af8cbc7deadfb01fe1314
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-19 18:19:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// script.js
+
+function activateSearch() {
+    var searchComponent = document.getElementById("searchComponent");
+    searchComponent.classList.add("active");
+
+    var searchInput = document.getElementById("searchInput");
+    searchInput.focus();
+}
